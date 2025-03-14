85f92abf333b7091750e3ffe245234ac03725ca4
alireza <alirezatt705@gmail.com>
2025-03-14 23:16:10 +0330

    dynamic segment in route handlers changed

--- src/app/api/tickets/[id]/route.ts
+++ src/app/api/tickets/[id]/route.ts
@@ -1,36 +1,36 @@
-// import { db } from "@/app/db/db";
-// import { NextResponse } from "next/server";
+import { db } from "@/app/db/db";
+import { NextResponse } from "next/server";
 
-// export async function GET(
-//   request: Request,
-//   { params }: { params: { id: string } }
-// ) {
-//   const { id } = params;
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
 
-//   const ticket = await db.ticket.findUnique({
-//     where: { id },
-//   });
+  const ticket = await db.ticket.findUnique({
+    where: { id },
+  });
 
-//   if (!ticket) {
-//     return NextResponse.json({ message: "Ticket not found" }, { status: 404 });
-//   }
+  if (!ticket) {
+    return NextResponse.json({ message: "Ticket not found" }, { status: 404 });
+  }
 
-//   return NextResponse.json(ticket);
-// }
+  return NextResponse.json(ticket);
+}
 
-// export async function PUT(
-//   request: Request,
-//   { params }: { params: { id: string } }
-// ) {
-//   const { id } = params;
-//   // const { response } = await request.json();
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  // const { response } = await request.json();
 
-//   const updatedTicket = await db.ticket.update({
-//     where: { id },
-//     data: {
-//       status: "closed",
-//     },
-//   });
+  const updatedTicket = await db.ticket.update({
+    where: { id },
+    data: {
+      status: "closed",
+    },
+  });
 
-//   return NextResponse.json(updatedTicket);
-// }
+  return NextResponse.json(updatedTicket);
+}
